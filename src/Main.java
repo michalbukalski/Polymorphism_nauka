651bbd9ce5b26a4d2afe66f28347e43ceceb979b
michalbukalski <michalbukalski@gmail.com>
2025-03-20 21:44:25 +0100

    dodanie polimorfizmu inny sposób

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Movie theMovie = new Comedy("Star Wars");
+        Movie theMovie = new Adventure("Star Wars");
         theMovie.watchMovie();
+
+        Movie theMovie2 = Movie.getMovie("Adventure", "Star Wars");
+        theMovie2.watchMovie();
     }
 }
